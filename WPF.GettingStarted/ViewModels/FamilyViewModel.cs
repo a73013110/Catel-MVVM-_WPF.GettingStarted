26096e8ce0e0e5b23bd54c3cb95a7e833eee3b9a
王義凱 <a73013110@gmail.com>
2018-12-21 17:14:41 +0800

    Learning View Models

---
+++ WPF.GettingStarted/ViewModels/FamilyViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Catel;
+using Catel.Data;
+using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.GettingStarted.Models;
+
+namespace WPF.GettingStarted.ViewModels
+{
+    class FamilyViewModel : ViewModelBase
+    {
+        public FamilyViewModel(Family family)
+        {
+            Argument.IsNotNull(() => family);
+            Family = family;
+        }
+
+        public static readonly PropertyData FamilyProperty = RegisterProperty("Family", typeof(Family), null);
+        [Model]
+        public Family Family
+        {
+            get { return GetValue<Family>(FamilyProperty); }
+            private set { SetValue(FamilyProperty, value); }
+        }
+
+        public static readonly PropertyData PersonsProperty = RegisterProperty("Persons", typeof(ObservableCollection<Person>), null);
+        [ViewModelToModel("Family")]
+        public ObservableCollection<Person> Persons
+        {
+            get { return GetValue<ObservableCollection<Person>>(PersonsProperty); }
+            private set { SetValue(PersonsProperty, value); }
+        }
+
+        public static readonly PropertyData FamilyNameProperty = RegisterProperty("FamilyName", typeof(string));
+        [ViewModelToModel("Family")]
+        public string FamilyName
+        {
+            get { return GetValue<string>(FamilyNameProperty); }
+            set { SetValue(FamilyNameProperty, value); }
+        }
+    }
+}
--- WPF.GettingStarted/ViewModels/PersonViewModel.cs
+++ WPF.GettingStarted/ViewModels/PersonViewModel.cs
@@ -1,35 +1,45 @@
-﻿using System;
+﻿using Catel;
+using Catel.Data;
+using Catel.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF.GettingStarted.Models;
 
-namespace WPF.GettingStarted.Models
+namespace WPF.GettingStarted.ViewModels
 {
-    using Catel.Data;
-    using Catel.MVVM;
-
-    public class Person : ValidatableModelBase
+    class PersonViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Register "FirstName" property
-        /// </summary>
+        public PersonViewModel(Person person)
+        {
+            Argument.IsNotNull(() => person);
+            Person = person;
+        }
+
+        public static readonly PropertyData PersonProperty = RegisterProperty("Person", typeof(Person), null);
+        [Model]
+        public Person Person
+        {
+            get { return GetValue<Person>(PersonProperty); }
+            set { SetValue(PersonProperty, value); }
+        }
+
         public static readonly PropertyData FirstNameProperty = RegisterProperty("FirstName", typeof(string), null);
-        /// <summary>
-        /// Get/Set "FirstName"
-        /// </summary>
+        [ViewModelToModel("Person")]
         public string FirstName
         {
             get { return GetValue<string>(FirstNameProperty); }
             set { SetValue(FirstNameProperty, value); }
         }
+
         public static readonly PropertyData LastNameProperty = RegisterProperty("LastName", typeof(string), null);
+        [ViewModelToModel("Person")]
         public string LastName
         {
             get { return GetValue<string>(LastNameProperty); }
             set { SetValue(LastNameProperty, value); }
         }
-
-        public override string ToString() => FirstName + " " + LastName;
     }
 }
